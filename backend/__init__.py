1acd7459560a45d1fc5c914c770f0138ff4a8d00
chris-a-talbot <christopher.a.talbot@gmail.com>
2025-06-12 13:47:43 -0400

    Attempt to change deployment structure

--- backend/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-ARGscape backend package.
-""" 
--- backend/tskit_utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Tree Sequence utilities for ARGscape.
-"""
-
-from .io import load_tree_sequence_from_file
-
-__all__ = [
-    'load_tree_sequence_from_file'
-]
