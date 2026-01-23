cc3701e18040b58bb2cc305f68583066cb95aa66
chris-a-talbot <christopher.a.talbot@gmail.com>
2026-01-23 14:42:24 -0500

    Update requirements

--- argscape/__init__.py
+++ argscape/__init__.py
@@ -14,7 +14,7 @@ Then:
     result = argscape.infer(ts, method="fastgaia")
 """
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 from .visualize import visualize, VizResult
 from .spatial import is_spatial_available, is_playwright_available
